2ba37fc1b1bcb6efe61b705b663d2f5ee97fd38c
Tony1213 <1973265001@qq.com>
2016-07-15 16:11:35 -0600

    提交默认IP和端口

--- app/src/main/java/com/robot/et/business/MainActivity.java
+++ app/src/main/java/com/robot/et/business/MainActivity.java
@@ -48,8 +48,8 @@ public class MainActivity extends RosActivity {
 
 
 	public MainActivity(){
-//		super("XRobot","Xrobot",URI.create("http://192.168.3.1:11311"));//本体的ROS IP和端口
-		super("XRobot","Xrobot",URI.create("http://192.168.2.164:11311"));
+		super("XRobot","Xrobot",URI.create("http://192.168.3.1:11311"));//本体的ROS IP和端口
+//		super("XRobot","Xrobot",URI.create("http://192.168.2.164:11311"));
 	}
 
 	@Override
